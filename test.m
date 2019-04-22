4b658aea6a56867b3723878121eecaa80fecec8d
antoniooreany <antonio_oreany@mail.ru>
2019-04-23 01:28:07 +0200

    in test.m trying to solve z^pi==1

--- test.m
+++ test.m
@@ -11,11 +11,15 @@
 % axis equal
 
 
-x = sin (0: .01: 2 * pi);
-y = cos (0: .01: 2 * pi);
-plot (x, y);
-axis equal
+syms z                                                  % initialize variable
+eqn = z^pi==1;                                           % initialize equation
+sol = solve(eqn,z);                                     % initialize solutions
 
+% for i = 1:n
+%     fprintf('%f%+fj\n', real(sol(i)), imag(sol(i)));    % print solutions to Command Window 
+% end
+
+plot(sol, '+');  
 
 
 
