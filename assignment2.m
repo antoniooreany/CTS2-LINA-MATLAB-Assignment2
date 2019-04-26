4944b0444cb39b083ed8e9d8f22c3db82593b6cc
antoniooreany <antonio_oreany@mail.ru>
2019-04-26 18:16:19 +0200

    assignment2_function.m was renamed into assignment2.m

---
+++ assignment2.m
@@ -0,0 +1,78 @@
+function [] = assignment2(varargin)
+
+% Write a MATLAB-function that visualises all different n solutions 
+% to the equation z.^n = q, where z
+% and q are complex numbers and n is a positive integer.
+% The function you write needs to ask for n and q as input parameters 
+% and display all solutions on a circle line as output values; 
+% choose an appropriate scaling of both axis.
+% Furthermore your function needs to list all solutions to z.^n = q 
+% in their algebraic representation, giving their real and imaginary parts.
+% Test your generated MATLAB le with the following equation z.^7 = +2j, where j is the
+% imaginary unit, i.e. j.^2 = -1.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% function [t, y] = solver(varargin)
+n = varargin{1};
+q = varargin{2};
+if nargin < 2
+    options = [];
+else
+    options = varargin{4};
+end
+[t,y] = ode45(h_odefun, tspan, y0, options);
+end
+
+
+
+%%%%% First solution: Manual one %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+
+% z = zeros(1, n);
+% z(1) = q.^(1/n);
+% disp(z(1));
+% disp((z(1).^n));
+% 
+% for i = 1:n - 1
+%     z(i + 1) = z(1) * exp(1i * 2 * pi * i / n);
+%     disp(z(i + 1));
+%     disp((z(i + 1).^n));
+% end
+
+%%%%% Second solution: More general: Applicable for (the system of )(in)equalities %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+
+syms z                                                  % initialize variable
+eqn = z^n==q;                                           % initialize equation
+sol = solve(eqn,z);                                     % initialize solutions
+
+for i = 1:n
+    fprintf('%f%+fj\n', real(sol(i)), imag(sol(i)));    % print solutions to Command Window 
+end
+
+plot(sol, '+');                                         % draw the solutions
+hold on;        % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
+
+% TODO display all solutions on a circle line as output values; 
+
+radius = q.^(1/n);
+angle = 0: .01: 2 * pi;
+x = radius * sin (angle);
+y = radius * cos (angle);
+plot (x, y);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+axis equal;     % use equal data unit lengths along each axis.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% disp(sol);
+
+% TODO How to choose an appropriate scaling of both axis?
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+
+hold off;       % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
+
+end
+
