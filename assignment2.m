048351997b6bee904db910f391eaadeb0f0704d3
antoniooreany <antonio_oreany@mail.ru>
2019-04-26 18:57:55 +0200

    the code were cleaned up.

--- assignment2.m
+++ assignment2.m
@@ -11,18 +11,16 @@ function [] = assignment2(varargin)
 % Test your generated MATLAB le with the following equation z.^7 = +2j, where j is the
 % imaginary unit, i.e. j.^2 = -1.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%%%% Setting default values in case when no parameters are passed %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin < 2   
-    n = 7;              % set default values of equasion
-    q = 2;              % set default values of equasion
+    n = 7;                                              % set default values of equasion
+    q = 2;                                              % set default values of equasion
 else
-    n = varargin{1};    % set values of equation from the function parameters
-    q = varargin{2};    % set values of equation from the function parameters
+    n = varargin{1};                                    % set values of equation from the function parameters
+    q = varargin{2};                                    % set values of equation from the function parameters
 end
 
-%%%%% First solution: Manual one %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-
+%%%%% First solution: Manual one %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % z = zeros(1, n);
 % z(1) = q.^(1/n);
 % disp(z(1));
@@ -35,7 +33,6 @@ end
 % end
 
 %%%%% Second solution: More general: Applicable for (the system of )(in)equalities %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-
 syms z                                                  % initialize variable
 eqn = z^n==q;                                           % initialize equation
 sol = solve(eqn,z);                                     % initialize solutions
@@ -45,29 +42,20 @@ for i = 1:n
 end
 
 plot(sol, '+');                                         % draw the solutions
-hold on;        % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
+hold on;                                                % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
 
-% TODO display all solutions on a circle line as output values;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Draw a circle line: 
+radius = q.^(1/n);                                      % Initialize a radius of the circle.
+angle = 0: .01: 2 * pi;                                 % Initialize a array of angles for drawing the circle.
+x = radius * sin (angle);                               % Initialize a array of x-coordinates for drawing the circle.
+y = radius * cos (angle);                               % Initialize a array of y-coordinates for drawing the circle.
+plot (x, y);                                            % Draw all the points from the (x,y)-array.
 
-radius = q.^(1/n);
-angle = 0: .01: 2 * pi;
-x = radius * sin (angle);
-y = radius * cos (angle);
-plot (x, y);
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-axis equal;     % use equal data unit lengths along each axis.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% disp(sol);
+axis equal;                                             % use equal data unit lengths along each axis.
 
 % TODO How to choose an appropriate scaling of both axis?
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
-
-hold off;       % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
+hold off;                                               % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
 
 end
 
