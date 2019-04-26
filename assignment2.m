a53c7a12a428455bfe608487bab6841995a2aeab
antoniooreany <antonio_oreany@mail.ru>
2019-04-26 18:24:03 +0200

    the default values of equasion were set.

--- assignment2.m
+++ assignment2.m
@@ -1,30 +1,25 @@
 function [] = assignment2(varargin)
 
-% Write a MATLAB-function that visualises all different n solutions 
+% Write a MATLAB-function that visualises all different n solutions
 % to the equation z.^n = q, where z
 % and q are complex numbers and n is a positive integer.
-% The function you write needs to ask for n and q as input parameters 
-% and display all solutions on a circle line as output values; 
+% The function you write needs to ask for n and q as input parameters
+% and display all solutions on a circle line as output values;
 % choose an appropriate scaling of both axis.
-% Furthermore your function needs to list all solutions to z.^n = q 
+% Furthermore your function needs to list all solutions to z.^n = q
 % in their algebraic representation, giving their real and imaginary parts.
 % Test your generated MATLAB le with the following equation z.^7 = +2j, where j is the
 % imaginary unit, i.e. j.^2 = -1.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% function [t, y] = solver(varargin)
-n = varargin{1};
-q = varargin{2};
-if nargin < 2
-    options = [];
+if nargin < 2   
+    n = 7;              % set default values of equasion
+    q = 2;              % set default values of equasion
 else
-    options = varargin{4};
+    n = varargin{1};    % set values of equation from the function parameters
+    q = varargin{2};    % set values of equation from the function parameters
 end
-[t,y] = ode45(h_odefun, tspan, y0, options);
-end
-
-
 
 %%%%% First solution: Manual one %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 
@@ -32,7 +27,7 @@ end
 % z(1) = q.^(1/n);
 % disp(z(1));
 % disp((z(1).^n));
-% 
+%
 % for i = 1:n - 1
 %     z(i + 1) = z(1) * exp(1i * 2 * pi * i / n);
 %     disp(z(i + 1));
@@ -46,13 +41,13 @@ eqn = z^n==q;                                           % initialize equation
 sol = solve(eqn,z);                                     % initialize solutions
 
 for i = 1:n
-    fprintf('%f%+fj\n', real(sol(i)), imag(sol(i)));    % print solutions to Command Window 
+    fprintf('%f%+fj\n', real(sol(i)), imag(sol(i)));    % print solutions to Command Window
 end
 
 plot(sol, '+');                                         % draw the solutions
 hold on;        % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
 
-% TODO display all solutions on a circle line as output values; 
+% TODO display all solutions on a circle line as output values;
 
 radius = q.^(1/n);
 angle = 0: .01: 2 * pi;
